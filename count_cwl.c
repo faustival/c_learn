dfa6b05d741e324230ec061c71e1a65fe7e1e86d
anyonsites <xjin.xmu@gmail.com>
2017-04-13 21:20:19 +0800

    count chars, words, lines

---
+++ count_cwl.c
@@ -0,0 +1,54 @@
+/*
+ * K&R Sec. 1.5
+ * Check the result with:
+ *     ./cat_wc.exc < somefile > writefile
+ *     wc -l file
+ *     wc -m file
+ *     wc -w file
+ */
+
+#include <stdio.h>
+
+#define on 1
+#define off 0
+
+int cat_wc();
+
+int main()
+{
+    cat_wc();
+    return 0;
+}
+
+int cat_wc()
+{ // could be a simple version of Linux cat, wc
+    int c;
+    int trigger;
+    long nchar, nline, nword;
+
+    nchar = nline = 0;
+    trigger = off;
+    while ( (c = getchar()) != EOF )
+    {
+        putchar(c);
+        //fflush(stdout);
+        //printf("%ld\n", c);
+        if (c == '\n')
+            ++nline;
+        if (c == '\n' || c == ' ' || c == '\t')
+            trigger = off;
+        else
+            if (trigger == off)
+            {
+                ++nword;
+                trigger = on;
+            }
+        ++nchar;
+    }
+    printf("N_line, %ld\n", nline);
+    printf("N_char, %ld\n", nchar);
+    printf("N_word, %ld\n", nword);
+}
+
+
+
