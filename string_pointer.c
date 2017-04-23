f090597e4943a28d639631b8a2255d3409212016
anyonsites <xjin.xmu@gmail.com>
2017-04-24 02:12:12 +0800

    loop string pointer

---
+++ string_pointer.c
@@ -0,0 +1,47 @@
+/*
+ * K&R Sec. 5.3
+ *
+ * String as character arrays,
+ * loop by pointer and subscription is also the same.
+ *
+ */
+
+#include <stdio.h>
+
+int putstr(char *s);
+int putstr_sub(char *s);
+
+int main()
+{
+    char *s = "Running.";
+
+    putstr(s);
+    putstr_sub(s);
+}
+
+int putstr(char *s)
+{
+    int i;
+
+    printf("Loop by pointer:\n");
+
+    while (*s != '\0') 
+    {
+        putchar(*s);
+        printf("\n");
+        *s++;
+    }
+}
+
+int putstr_sub(char *s)
+{
+    int i;
+
+    printf("Loop by subscript:\n");
+
+    for (i=0; s[i] != '\0'; i++ ) 
+    {
+        putchar(s[i]);
+        printf("\n");
+    }
+}
